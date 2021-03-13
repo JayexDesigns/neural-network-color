0f324fd50f9a5db2b2b9bb56cee4bde2a82dfd5d
Joaquin Touron <43842304+JayexDesigns@users.noreply.github.com>
2021-03-13 03:22:24 +0100

    clicking the text in the center copies the color codes, gui probably perfect in pc, needs tweaks in mobile

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ var div1 = document.getElementById("color1");
 var div2 = document.getElementById("color2");
 var text1 = document.getElementById("colorText1");
 var text2 = document.getElementById("colorText2");
+var copyText = document.getElementById("copyText");
 
 var copy = document.getElementById("text");
 
@@ -33,6 +34,7 @@ function randomColors() {
     text2.style.color = color1;
     text1.innerText = `#${color1Nums[0].toString(16)}${color1Nums[1].toString(16)}${color1Nums[2].toString(16)}`;
     text2.innerText = `#${color2Nums[0].toString(16)}${color2Nums[1].toString(16)}${color2Nums[2].toString(16)}`;
+    copyText.value = `${text1.innerText},${text2.innerText}`
 }
 
 function chColor1() {
@@ -57,11 +59,29 @@ no.addEventListener("click", randomColors);
 
 
 
-// copy.addEventListener("mouseover", () => {
-//     let container = document.getElementById("controls");
-//     let ;
-//     container.appendChild();
-// });
+copy.addEventListener("mouseover", () => {
+    document.getElementById("copy").style.opacity = 1;
+    document.getElementsByTagName("svg")[0].style.opacity = 1;
+});
 
-// copy.addEventListener("mouseout", () => {
-// });
+copy.addEventListener("mouseout", () => {
+    document.getElementById("copy").style.opacity = 0;
+    document.getElementsByTagName("svg")[0].style.opacity = 0;
+});
+
+copy.addEventListener("click", () => {
+    copyText.select();
+    copyText.setSelectionRange(0,9999);
+    document.execCommand("copy");
+
+    document.getElementById("copy").style.opacity = 1;
+    document.getElementsByTagName("svg")[0].style.opacity = 1;
+    document.getElementById("copy").innerHTML = "Copied!";
+    setTimeout(() => {
+        document.getElementById("copy").style.opacity = 0;
+        document.getElementsByTagName("svg")[0].style.opacity = 0;
+    }, 750);
+    setTimeout(() => {
+        document.getElementById("copy").innerHTML = "Copy?";
+    }, 1000);
+});
